f32e2454eccd707575117cf3249d17708bf2febe
Brendan Collins <brendan.c.collins@gmail.com>
2017-07-28 17:18:14 -0400

    initial commit

---
+++ GruntFile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {  
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    zip: {
+      'build': {
+        src: ["*.html","css/*.*", "js/*.js", "fonts/*.*","icon.svg","package.json"],
+        dest: 'build/<%= pkg.name %>_v<%= pkg.version %>.fma'
+      }
+    },
+    clean:{
+      current_version:{
+        src:'build/<%= pkg.name %>_v<%= pkg.version %>.fma'
+        },
+      all:{
+        src:'build'
+        }
+      },
+  });
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-zip');
+  grunt.registerTask('clean',function(arg){
+    grunt.file.delete(grunt.config('clean.current_version.src'));
+  });
+  grunt.registerTask('clean_all',function(arg){
+    grunt.file.delete(grunt.config('clean.all.src'));
+  });
+  grunt.registerTask('build', ['clean','zip']);
+};
---
+++ js/main.js
@@ -0,0 +1,6 @@
+var dropdown = document.getElementById('options');
+var add = document.getElementById('add');
+
+add.onclick = function(){
+    dropdown.classList.toggle('is-active');
+};
